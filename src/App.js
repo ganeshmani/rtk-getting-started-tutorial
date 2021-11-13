2821eb7f785d457b7a89e60b2ef22679eb7312cc
ganeshmani <ganeshmani009@gmail.com>
2021-11-14 00:39:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 // import "./App.css";
 import './index.css';
-import Product from './features/Product'
+import Product from './features/product'
 function App() {
   return (
     <div className="container mx-auto p-4">
